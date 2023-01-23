21851755bd7bb0be9e9d0ae340d94c9ccddab1c1
isturiz <isturizmauricio@gmail.com>
2023-01-23 18:02:10 -0400

    fix: update ReactDOM v17 -> v18

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/index.js';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
 );
