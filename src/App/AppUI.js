e12f0c02320c2568a7639a5ce48e6fb6aed6eb7d
isturiz <isturizmauricio@gmail.com>
2023-01-30 04:14:35 -0400

    update: flex direction of container1

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -23,15 +23,18 @@ function AppUI() {
 
       <div className='container1'>
 
-        <h2 className="subtitle">Create New Task</h2>
-        <CreateTask
-          addTask={addTask} />
+        <div>
+
+          <h2 className="subtitle">Create New Task</h2>
+          <CreateTask
+            addTask={addTask} />
+        </div>
         <div className='relleno'></div>
         {/* <img src='https://assets.website-files.com/5e51c674258ffe10d286d30a/5e535cbcf5fa1a60fafcfcab_peep-95.svg' alt="life" /> */}
         {/* <img src='https://assets.website-files.com/5e51c674258ffe10d286d30a/5e535cf47488c27eb04a70d1_peep-97.svg' alt="life" /> */}
         <img src='https://assets.website-files.com/5e51c674258ffe10d286d30a/5e535bb6e35d38cae7684f8c_peep-86.svg' alt="life" />
 
-        
+
       </div>
 
       <div className='container2'>
