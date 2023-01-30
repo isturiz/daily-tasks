6e8835c5a55dadc027400817240a2933c3624f9b
isturiz <isturizmauricio@gmail.com>
2023-01-30 03:53:19 -0400

    add: responsive design, first version

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -27,9 +27,9 @@ function AppUI() {
         <CreateTask
           addTask={addTask} />
         <div className='relleno'></div>
-        <img src='https://assets.website-files.com/5e51c674258ffe10d286d30a/5e535cbcf5fa1a60fafcfcab_peep-95.svg' alt="life" />
+        {/* <img src='https://assets.website-files.com/5e51c674258ffe10d286d30a/5e535cbcf5fa1a60fafcfcab_peep-95.svg' alt="life" /> */}
         {/* <img src='https://assets.website-files.com/5e51c674258ffe10d286d30a/5e535cf47488c27eb04a70d1_peep-97.svg' alt="life" /> */}
-        {/* <img src='https://assets.website-files.com/5e51c674258ffe10d286d30a/5e535bb6e35d38cae7684f8c_peep-86.svg' alt="life" /> */}
+        <img src='https://assets.website-files.com/5e51c674258ffe10d286d30a/5e535bb6e35d38cae7684f8c_peep-86.svg' alt="life" />
 
         
       </div>
